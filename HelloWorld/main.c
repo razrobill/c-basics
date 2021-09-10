0c9e39b6dc301c26d150409efe6f47cf5b224a47
Talia Blain <tb1126@york.ac.uk>
2021-09-10 11:57:58 +0100

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Talia Blain on 10/09/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
